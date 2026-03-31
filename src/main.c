1aba4196ca08d709eaef524d2ad1e76ed862aa09
_arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2026-03-30 21:21:03 -0600

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <project-name.h>
+
+int WINAPI WinMain(
+	HINSTANCE appHandle,
+	HINSTANCE legacyPrevAppHandle,
+	LPSTR commandLine,
+	int windowShowFlags
+) {
+	(void)(legacyPrevAppHandle);
+	(void)(commandLine);
+	(void)(windowShowFlags);
+
+	// ...
+	return 0;
+}
